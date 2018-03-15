86ac4f976255f8b9b0fc57fea5a3a5bb3ac85d8a
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-03-15 15:56:42 +0800

    ...

--- app.js
+++ app.js
@@ -1522,6 +1522,7 @@ function ReplaceMessage(target, locale) {
 
 bot.dialog('/flow', [
     function (session, args) {
+        logger.info("session.userData.dialogs type:" + typeof(session.userData.dialogs));
         if(typeof(session.userData.dialogs) == String)session.userData.dialogs = JSON.parse(session.userData.dialogs);
         logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         logger.info('session conversationData: ' + JSON.stringify(session.conversationData));
