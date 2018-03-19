f86c921e32f843e2a1e3f38e907ecc039a4ddafb
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-03-19 14:39:58 +0800

    debug for edit_2

--- app.js
+++ app.js
@@ -350,7 +350,7 @@ app.put('/dialog/:dialog_id/:flow_id', function (request, response) {
     }
 });
 function UP(x, y) {
-    return (x[dialog_id] > y[dialog_id]) ? 1 : -1
+    return (x["dialog_id"] > y["dialog_id"]) ? 1 : -1
 
 }
 app.delete('/dialog/:dialog_id/:flow_id', function (request, response) {
