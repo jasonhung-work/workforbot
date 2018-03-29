07fb64113d280fbb4c75905d02e505f32b14f3ff
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-03-29 10:07:40 +0800

    ???

--- app.js
+++ app.js
@@ -1799,12 +1799,14 @@ bot.dialog('/flow', [
                 session.replaceDialog(redirect_dialog(session.userData.userId, session.conversationData.index), session.conversationData);
             }
         } else if (dialog.type == 'image') {
+            console.log('dialog: ' + dialog);
             builder.Prompts.attachment(session, dialog.prompt);
             userConversationMessage.push({ type: 'message', acct: 'flowbot', message: dialog.prompt });
             preventMessage.set(session.userData.userId, userConversationMessage);
             logger.info('session.send: ' + JSON.stringify(dialog.prompt));
             if (dialog.next < 0) session.conversationData.index = dialog.next;
             else session.conversationData.index = dialog.next_id;
+            logger.info('index: ' + session.conversationData.index);
             if (redirect_dialog(session.userData.userId, session.conversationData.index) == 'endDialog') {
                 session.endDialogWithResult({ response: session.conversationData.form });
                 if (session.message.address.channelId == 'directline') {
