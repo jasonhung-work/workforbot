14fda40562da05c6aa76f8ec40f040de93651504
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-04-03 13:40:22 +0800

    ???

--- app.js
+++ app.js
@@ -2553,6 +2553,25 @@ var checkRequiresToken = function (message) {
     return message.source === 'skype' || message.source === 'msteams';
 };
 
+function createHeroCard(session, dialog) {
+    var herocard = new builder.HeroCard(session)
+        .title(dialog.attachments[0].content.title)
+        .subtitle(dialog.attachments[0].content.subtitle)
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images([
+            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+        .buttons([
+        ]);
+    for (var index = 0; index < dialog.attachments[0].content.buttons.length; index++) {
+        if(dialog.attachments[0].content.buttons.type == "postBack")
+        herocard.buttons.push(builder.CardAction.imBack(session, dialog.attachments[0].content.buttons[index].title, session, dialog.attachments[0].content.buttons[index].value));
+        else if(dialog.attachments[0].content.buttons.type == "url")
+        herocard.buttons.push(builder.CardAction.openUrl(session, dialog.attachments[0].content.buttons[index].url));
+    }
+    return 
+}
+
 function redirect_dialog(userId, end_point) {
     var result;
     switch (end_point.toString()) {
